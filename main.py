a434cb728711f663c31cb10306a81600ea21bd1f
awais-web <ahmad.webdevelopment@gmail.com>
2022-08-26 17:41:42 +0500

    new list of 77 mode has been created

--- main.py
+++ main.py
@@ -2,4 +2,7 @@
 # create a list of 1000 integers and create new list of numbers divisible by 77 and than find its quantity
 
 
-list1 = lsit(range(1,1001))
+list1 = list(range(1,1001))
+
+newlist = [x for x in list1 if x % 77 == 0]
+print(newlist)
